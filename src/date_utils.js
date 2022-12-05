850086eaccbcbea8cee3f0a6c4a575e6b744d2e0
Raymond Leow <raymond.leow@infor.com>
2022-12-05 14:51:06 +0100

    'WIP'

--- src/date_utils.js
+++ src/date_utils.js
@@ -252,7 +252,7 @@ export default {
             scale += 's';
         }
 
-        return Math.floor(
+        return Math.ceil(
             {
                 milliseconds,
                 seconds,
