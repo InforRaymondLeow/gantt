c91f34adde4e3eb360ab846c7f6b8a175703a5fc
Raymond Leow <raymond.leow@infor.com>
2024-01-24 11:42:07 +0100

    'WIP'

--- src/popup.js
+++ src/popup.js
@@ -25,14 +25,14 @@ export default class Popup {
             throw new Error('target_element is required to show popup');
         }
 
-        
         const target_element = options.target_element;
 
         if (this.custom_html) {
             let html = this.custom_html(options.task);
-            html += '<div class="pointer"></div>';
+            // html += '<div class="pointer"></div>';
+            // html += '<div class="pointer"></div>';
             this.parent.innerHTML = html;
-            this.pointer = this.parent.querySelector('.pointer');
+            this.pointer = this.parent.querySelector('.arrow');
         } else {
             // set data
             this.title.innerHTML = options.title;
@@ -48,29 +48,36 @@ export default class Popup {
             position_meta = options.target_element.getBBox();
         }
 
-        const margin = 10
-        const right_side_space = (this.grid_size.width - position_meta.x - 
-            position_meta.width - this.parent.clientWidth)
+        const margin = 10;
+        const right_side_space =
+            this.grid_size.width -
+            position_meta.x -
+            position_meta.width -
+            this.parent.clientWidth;
         if (margin < right_side_space) {
             options.position = 'right';
         } else {
-            options.position = 'left'
+            options.position = 'left';
         }
 
         if (options.position === 'left') {
-            this.parent.style.left = 
-                (position_meta.x - this.parent.clientWidth - 10) + 'px';
-            this.parent.style.top = (position_meta.y - this.parent.clientHeight + position_meta.height) + 'px';
+            this.parent.style.left =
+                position_meta.x - this.parent.clientWidth - 10 + 'px';
+            this.parent.style.top =
+                position_meta.y -
+                this.parent.clientHeight +
+                position_meta.height +
+                'px';
 
-            this.pointer.style.transform = 'rotateZ(270deg)';
-            this.pointer.style.left = (this.parent.clientWidth + 7) + 'px';
-            this.pointer.style.top = (this.parent.clientHeight - 10 - 2) + 'px';
+            // this.pointer.style.transform = 'rotateZ(270deg)';
+            this.pointer.style.left = this.parent.clientWidth + 7 + 'px';
+            this.pointer.style.top = this.parent.clientHeight - 10 - 2 + 'px';
         } else {
             this.parent.style.left =
                 position_meta.x + (position_meta.width + 10) + 'px';
             this.parent.style.top = position_meta.y + 'px';
 
-            this.pointer.style.transform = 'rotateZ(90deg)';
+            // this.pointer.style.transform = 'rotateZ(90deg)';
             this.pointer.style.left = '-7px';
             this.pointer.style.top = '2px';
         }
